b746fb590702ab2a66f29cb757b78d86a4644202
AnthonyEstelaF <estela.flores.antony.rober.oswaldo@iisgalvanimi.edu.it>
2021-03-23 09:53:45 +0000

    connect serv

---
+++ client-socketio/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'https://3000-jade-stoat-99zubrsh.ws-eu03.gitpod.io', options: {} };
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
